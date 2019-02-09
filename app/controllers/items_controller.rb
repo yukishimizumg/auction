66d6dd2ec2a680c6bf58842fd108f2ec222832cc
yukishimizumg <marbleday.yukimay8@gmail.com>
2019-02-09 17:28:32 +0900

    edit,update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,17 @@ class ItemsController < ApplicationController
     redirect_to @item
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to @item
+  end
+
+
   private
 
   def item_params
