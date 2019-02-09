4fa04a826e96d5a4152cc5c52c820aefca6ddeb1
yukishimizumg <marbleday.yukimay8@gmail.com>
2019-02-09 16:37:34 +0900

    comp

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,24 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def index
     @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
+  end
 end
